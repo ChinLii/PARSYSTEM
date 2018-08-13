42bdc46761bc9f5b33de529f6d74ee4b3f768033
ChinLi <kananek.ati@hotmail.com>
2018-08-13 22:45:27 +0100

    add test

--- PARecognition/app/routers/apiRouter.js
+++ PARecognition/app/routers/apiRouter.js
@@ -19,6 +19,16 @@ router.post('/upload',upload.single('file'),function(req,res){
     });
 })
 
+router.get('/getHistory',function(req,res){
+    History.find({},{},{sort: { '_id': -1 }},function(err,result){
+        if(err){
+            res.status(404).render("error",{message: "Something went wrong in History database. ", redirect: ""});
+        }else{
+            res.status(200).json({data : result});
+        }
+    }).sort({ date: -1 }) 
+})
+
 router.post('/updateName',function(req,res){
     History.update({'_id': req.body.id}, {$set : {'name': req.body.name}},function(err,result){
         if(err){
@@ -32,6 +42,9 @@ router.post('/updateName',function(req,res){
 
 router.post('/save',function(req,res){
     var tempHistory = new History();
+    if(req.body._id){
+        tempHistory._id = req.body._id;
+    }
     tempHistory.name = req.body.name;
     tempHistory.activitiesList= req.body.activitiesList.split(",");
     tempHistory.timestamp = req.body.timestamp.split(",");
--- PARecognition/test/test.js
+++ PARecognition/test/test.js
@@ -15,50 +15,97 @@ global.should = chai.should();
 
 chai.use(chaiHttp);
 
+ /*
+  * Test the API request
+  */
+ describe('/POST API METHOD', ()=> {
+    it("It will create a new history",function(done){
+        api.post("/api/save")
+        .send({  
+            "activitiesList":  "Standing,Standing,Standing,Standing",
+            "timestamp":"1200000000200,1200000000200,1200000000200,1200000000200",
+            "xAxis": "0.08283484688138983,0.08283484688138983,0.08283484688138983,0.08283484688138983",
+            "yAxis":"1.7820365526511324,1.7820365526511324,1.7820365526511324,1.7820365526511324,1.7820365526511324",
+            "zAxis":"-0.6047672794785012,-0.6047672794785012,-0.6047672794785012,-0.6047672794785012,-0.6047672794785012",
+            "_id":"5b71f2cd31e6cf257450450d",
+            "name":"AutoTest",
+         })
+        .end(function(err,res){
+            res.should.have.status(200);
+            done();
+        })
+    })
+    it("It will change the name of the history by ID ",function(done){
+        api.post('/api/updateName')
+        .send({
+            "id" : "5b71f2cd31e6cf257450450d",
+            "name" : "AutoTestChanged",
+        })
+        .end(function(err,res){
+            res.should.have.status(200);
+            done();
+        })
+    })
+})
+
 /*
   * Test the HTML PAGES
   */
  describe('/TEST HTML PAGES', ()=> {
-     it("It will render the home page",function(done){
-         api.get("/")
-         .expect(200)
-         .end(function(err,res){
-             res.should.have.status(200);
-             done();
-         })
-     });
-     it("It will render the contact page",function(done){
-        api.get("/contact")
+    it("It will render the home page",function(done){
+        api.get("/")
         .expect(200)
         .end(function(err,res){
             res.should.have.status(200);
             done();
         })
     });
-    it("It will render the history page",function(done){
-        api.get("/history")
-        .expect(200)
-        .end(function(err,res){
-            res.should.have.status(200);
-            done();
-        })
-    });
-    it("It will render the system page",function(done){
-        api.get("/system")
-        .expect(200)
-        .end(function(err,res){
-            res.should.have.status(200);
-            done();
-        })
-    });
-    it("It will render the load parsystem page on this id : 5b6a2a47b0fc2421e4d47676",function(done){
-        api.get("/load/parsystem/5b6a2a47b0fc2421e4d47676")
-        .expect(200)
-        .end(function(err,res){
-            res.should.have.status(200);
-            done();
-        })
-    });
+    it("It will render the contact page",function(done){
+       api.get("/contact")
+       .expect(200)
+       .end(function(err,res){
+           res.should.have.status(200);
+           done();
+       })
+   });
+   it("It will render the history page",function(done){
+       api.get("/history")
+       .expect(200)
+       .end(function(err,res){
+           res.should.have.status(200);
+           done();
+       })
+   });
+   it("It will render the system page",function(done){
+       api.get("/system")
+       .expect(200)
+       .end(function(err,res){
+           res.should.have.status(200);
+           done();
+       })
+   });
+   it("It will render the load parsystem page on this id : 5b71f2cd31e6cf257450450d",function(done){
+       api.get("/load/parsystem/5b71f2cd31e6cf257450450d")
+       .expect(200)
+       .end(function(err,res){
+           res.should.have.status(200);
+           done();
+       })
+   });
 
- })
-
+})
+/*
+  * Test the Delete History
+  */
+describe('/DELETE API METHOD', ()=> {
+    it("It will delete the history by ID ",function(done){
+        api.post("/api/deleteHistory")
+        .send({
+            "id" : "5b71f2cd31e6cf257450450d"
+        })
+        .end(function(err,res){
+            res.should.have.status(200);
+            done();
+        })
+    });
+})
